031aa767ed28b7e05d33e62ffce876c7ff2731f4
J-Michael Roberts <jroberts@corvusforensics.com>
2024-02-19 14:01:10 -0500

    add bhupdate with github urls

---
+++ script/bhupdate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# BlackHarrier11 Updater script
+
+# Designed to handle the updating of all the things beyond apt-get update
+
+# Die on error
+set -e
+
+# update the repo based stuff
+
+apt-get update
+#apt-get -y upgrade
+apt-get -y full-upgrade
+apt-get -y autoremove
+apt-get clean
+
+# update the BH11 Scripts
+# TODO - add checks to scripts and sources
+wget -O /usr/local/sbin/bhpwdchk https://github.com/Corvus4n6/blackharrier/raw/master/script/bhpwdchk.sh
+chmod +x /usr/local/sbin/bhpwdchk
+wget -O /usr/local/sbin/bhreplicate https://github.com/Corvus4n6/blackharrier/raw/master/script/bhreplicate.sh
+chmod +x /usr/local/sbin/bhreplicate
+wget -O /usr/local/sbin/bhupdate https://github.com/Corvus4n6/blackharrier/raw/master/script/bhupdate.sh
+chmod +x /usr/local/sbin/bhupdate
